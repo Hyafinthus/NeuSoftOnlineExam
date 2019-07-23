a5959e1744f4dd7e2a1ca9f19a622fc575a8bb5c
e <hoyifu@gmail.com>
2019-07-23 11:25:44 +0800

    Init

---
+++ src/main/java/com/neuedu/exam/NeuSoftOnlineExamApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuSoftOnlineExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuSoftOnlineExamApplication.class, args);
+	}
+
+}
