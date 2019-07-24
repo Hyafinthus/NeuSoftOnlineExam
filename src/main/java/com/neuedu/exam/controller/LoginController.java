58486b90b7936cf47be5d5006edf321f1b7fb62f
e <hoyifu@gmail.com>
2019-07-24 14:49:03 +0800

    teacher login debug

--- src/main/java/com/neuedu/exam/controller/LoginController.java
+++ src/main/java/com/neuedu/exam/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.neuedu.exam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,7 +26,9 @@ public class LoginController {
 	
 	@RequestMapping(value="/teacher", method=RequestMethod.POST)
 	@ResponseBody
-	public String teacherLogin(User user) {
+	public String teacherLogin(@RequestBody User user) {
+		System.out.println("进入controller");
+		System.out.println(user);
 		Teacher teacher = loginService.teacherLogin(user);
 		if (teacher != null) {
 			return "success";
