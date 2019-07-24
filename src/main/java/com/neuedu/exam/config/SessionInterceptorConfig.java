2cc282e05468850ccfc999f95e8220a60ae1ef39
e <hoyifu@gmail.com>
2019-07-24 18:30:11 +0800

    Interceptor

--- src/main/java/com/neuedu/exam/config/SessionInterceptorConfig.java
+++ src/main/java/com/neuedu/exam/config/SessionInterceptorConfig.java
@@ -1,5 +1,8 @@
 package com.neuedu.exam.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,9 +12,12 @@ import com.neuedu.exam.interceptor.SessionInterceptor;
 @Configuration
 public class SessionInterceptorConfig implements WebMvcConfigurer {
 
+	List<String> include = Arrays.asList("/**");
+	List<String> exclude = Arrays.asList("/js/**", "/css/**", "/fonts/**", "/img/**", "/index.html", "/login/**");
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/exam");
+		registry.addInterceptor(new SessionInterceptor()).addPathPatterns(include).excludePathPatterns(exclude);
 	}
 	
 }
