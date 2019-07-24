e201c3f7bc033fd4f9054a63e6e817b91d56c18b
e <hoyifu@gmail.com>
2019-07-24 17:01:40 +0800

    Personal COntroller

---
+++ src/main/java/com/neuedu/exam/controller/MenuController.java
@@ -0,0 +1,29 @@
+package com.neuedu.exam.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+	
+	@RequestMapping(value="/personal", method=RequestMethod.POST)
+	@ResponseBody
+	public List<String> getPersonal(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+		String name = (String) session.getAttribute("name");
+		return Arrays.asList(id, name);
+	}
+	
+	
+}
