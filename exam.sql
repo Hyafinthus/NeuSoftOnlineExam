0b448ad5e532729be3e807843a153e7f412e2561
e <hoyifu@gmail.com>
2019-07-29 13:46:09 +0800

    TODO Debug

--- exam.sql
+++ exam.sql
@@ -63,7 +63,7 @@ CREATE TABLE `course_student_table` (
 DROP TABLE IF EXISTS `course_table`;
 
 CREATE TABLE `course_table` (
-  `course_id` int(8) NOT NULL AUTO_INCREMENT,
+  `course_id` varchar(20) NOT NULL,
   `course_name` varchar(20) NOT NULL,
   `course_intro` varchar(100) NOT NULL,
   PRIMARY KEY (`course_id`)
@@ -80,7 +80,7 @@ CREATE TABLE `course_teacher_table` (
   `course_id` varchar(20) NOT NULL,
   `teacher_id` varchar(20) NOT NULL,
   PRIMARY KEY (`pri_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `course_teacher_table` */
 
@@ -191,7 +191,7 @@ CREATE TABLE `user_table` (
 
 /*Data for the table `user_table` */
 
-insert  into `user_table`(`user_id`,`user_name`,`user_password`,`user_group`) values ('A0001','于乃夫','yunaifu','admin'),('A0002','汪清瀛','wangqingying','admin'),('S0001','何一夫','heyifu','student'),('S0002','万一鸣','wanyiming','student'),('S0003','高祖琛','gaozuchen','student'),('S0004','任永鹏','renyongpeng','student'),('S0005','曾钰城','zengyucheng','student'),('S0006','黄项宇轩','huangxiangyuxuan','student'),('S0007','张锦航','zhangjinhang','student'),('S0008','杜子健','duzijian','student'),('S0009','王腾','wangteng','student'),('T0001','刘伟','123','teacher'),('T0002','王忠杰','wangzhongjie','teacher'),('T0003','徐汉川','xuhanchuan','teacher'),('T0004','吴锐','wurui','teacher'),('T0005','刘宏伟','liuhongwei','teacher'),('T0006','史先俊','shixianjun','teacher');
+insert  into `user_table`(`user_id`,`user_name`,`user_password`,`user_group`) values ('A0001','于乃夫','yunaifu','admin'),('A0002','汪清瀛','wangqingying','admin'),('S0001','何一夫','heyifu','student'),('S0002','万一鸣','wanyiming','student'),('S0003','高祖琛','gaozuchen','student'),('S0004','任永鹏','renyongpeng','student'),('S0005','曾钰城','zengyucheng','student'),('S0006','黄项宇轩','huangxiangyuxuan','student'),('S0007','张锦航','zhangjinhang','student'),('S0008','杜子健','duzijian','student'),('S0009','王腾','wangteng','student'),('T0001','刘伟','234','teacher'),('T0002','王忠杰','wangzhongjie','teacher'),('T0003','徐汉川','xuhanchuan','teacher'),('T0004','吴锐','wurui','teacher'),('T0005','刘宏伟','liuhongwei','teacher'),('T0006','史先俊','shixianjun','teacher');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
--- src/main/java/com/neuedu/exam/controller/CourseController.java
+++ src/main/java/com/neuedu/exam/controller/CourseController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,9 +24,14 @@ public class CourseController {
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	@ResponseBody
 	public String addCourse(String course_id, String course_name, String course_intro, HttpServletRequest request, HttpServletResponse response) {
+		System.out.println(course_id + course_name + course_intro);
+		
 		Course course = new Course(course_id, course_name, course_intro);
 		String id = (String) request.getSession().getAttribute("id");
-		TeacherCourse tc = new TeacherCourse(id, course_id);
+		TeacherCourse tc = new TeacherCourse(course_id, id);
+		
+		System.out.println(course + " " + tc);
+		
 		courseService.addCourse(course, tc);
 		return "success";
 	}
